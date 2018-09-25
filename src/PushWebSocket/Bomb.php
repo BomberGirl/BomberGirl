3dfd47d37a29d4a0e67c5554869140af98d2c0a1
denisr_15 <d.rykart@hispeed.ch>
2018-09-25 09:12:19 +0200

    Bomb-Class Attributes, Getter & Setter

---
+++ src/PushWebSocket/Bomb.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dryka
+ * Date: 18.09.2018
+ * Time: 10:43
+ */
+
+namespace PushWebSocket;
+
+
+class Bomb
+{
+    public $range;
+    public $timeToExplosion;
+    public $positionX;
+    public $positionY;
+
+    /**
+     * @return mixed
+     */
+    public function getRange()
+    {
+        return $this->range;
+    }
+
+    /**
+     * @param mixed $range
+     */
+    public function setRange($range)
+    {
+        $this->range = $range;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+     * @param mixed $time
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPositionX()
+    {
+        return $this->positionX;
+    }
+
+    /**
+     * @param mixed $positionX
+     */
+    public function setPositionX($positionX)
+    {
+        $this->positionX = $positionX;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPositionY()
+    {
+        return $this->positionY;
+    }
+
+    /**
+     * @param mixed $positionY
+     */
+    public function setPositionY($positionY)
+    {
+        $this->positionY = $positionY;
+    }
+
+
+}
